0121afc12d7b4846b0f610fa3923893d9f04c68f
Enrico Lionello <enrico.lionello@external.pagopa.it>
2026-03-28 13:01:49 +0100

    feat(go-json-parser): scaffold script structure and config

---
+++ scripts/go/go-json-parser/src/config.ts
@@ -0,0 +1,38 @@
+import { Core } from '@go-automation/go-common';
+
+export const scriptMetadata: Core.GOScriptMetadata = {
+  name: 'GO JSON Parser',
+  version: '1.0.0',
+  description: 'Estrattore di campi da file JSON/NDJSON con ricerca ricorsiva',
+  authors: ['Team GO - Gestione Operativa'],
+};
+
+export const scriptParameters: ReadonlyArray<Core.GOConfigParameterOptions> = [
+  {
+    name: 'input.file',
+    type: Core.GOConfigParameterType.STRING,
+    description: 'Percorso del file JSON o NDJSON in ingresso',
+    required: true,
+    aliases: ['i'],
+  },
+  {
+    name: 'field',
+    type: Core.GOConfigParameterType.STRING,
+    description: 'Campo da estrarre (supporta dot-notation o ricerca per chiave)',
+    required: true,
+    aliases: ['f'],
+  },
+  {
+    name: 'output.file',
+    type: Core.GOConfigParameterType.STRING,
+    description: 'Percorso del file di output (TXT)',
+    required: false,
+    aliases: ['o'],
+  },
+] as const;
+
+export interface GoJsonParserConfig {
+  readonly inputFile: string;
+  readonly field: string;
+  readonly outputFile: string | undefined;
+}
