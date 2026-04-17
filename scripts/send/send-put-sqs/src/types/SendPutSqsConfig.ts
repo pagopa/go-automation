f715e00f23cba28008badd63c9efca632b807528
Enrico Lionello <enrico.lionello@external.pagopa.it>
2026-04-17 15:12:51 +0200

    feat(send-put-sqs): add configuration types

---
+++ scripts/send/send-put-sqs/src/types/SendPutSqsConfig.ts
@@ -0,0 +1,11 @@
+export interface SendPutSqsConfig {
+  readonly queueUrl: string;
+  readonly inputFile: string;
+  readonly fileFormat: 'text' | 'json' | 'csv' | 'auto';
+  readonly csvColumn: string;
+  readonly batchSize: number;
+  readonly delaySeconds: number;
+  readonly batchMaxRetries: number;
+  readonly fifoGroupId?: string;
+  readonly fifoDeduplicationStrategy: 'content' | 'hash';
+}
